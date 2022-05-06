7b9fa66a802b5c843ea3d6bcca50cc7646604dba
fabnun <fabnun@gmail.com>
2022-05-06 11:00:35 -0400

    Se implemento el componente Config -> vuex

--- src/components/Config.vue
+++ src/components/Config.vue
@@ -3,11 +3,11 @@
     <p><strong>JOBWUS</strong> es una herramienta para hojear ofertas laborales extraídas desde bolsas de trabajo online, las cuales son agrupadas por similitud, ordenadas por fecha, permitiendo tambien búsquedas, marcaje de favoritos y archivado <a target="_blank" style="color: white" @click.stop="" href="https://github.com/fabnun/vue-jobwus">(ver en github)</a></p>
     <br />
     <h3><strong>Palabras Clave (extracción)</strong></h3>
-    <span>{{ words }}</span>
+    <span>{{ words }}.</span>
     <br /><br />
     <h3><strong> Configuración</strong></h3>
     <input type="checkbox" @click.stop="" id="ignorarTildes" v-model="ignorarTildes" />
-    <label class="menu-button" @click.stop="" for="ignorarTildes">Ignorar tildes</label>
+    <label class="menu-button" @click.stop="" for="ignorarTildes">Ignorar tildes en la busqueda.</label>
     <br />
     <br />
     <!-- <h2><strong>Recursos</strong></h2>
